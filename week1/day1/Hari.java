a2f30c47513850f978fa49edc6831a70ce2eaff7
hariaki18 <haridosshari4@gmail.com>
2025-08-05 12:04:58 -0400

    Add files via upload
    Week1

---
+++ week1/day1/Hari.java
@@ -0,0 +1,37 @@
+package week1.day1;
+
+public class Hari {
+	
+	public void testleaf(){
+		int a=12;
+		int b =25;
+		int add=a+b;
+		System.out.println(add
+				);
+		
+	}
+public void testlea2() {
+	int h=3;
+	int j=3;
+	int multiplay=h*j; 
+	System.out.println(multiplay);
+}
+public void testleaf3() {
+	int d=10;
+	int c=2;
+	int divide=d/c;
+	System.out.println(divide);
+}
+
+
+public static void main (String []args) {
+	System.out.println("welcome to testleaf" );
+	  	Hari test1=new Hari();
+	  	test1.testleaf();
+	  	test1.testlea2();
+	  	test1.testleaf3();
+	  	
+	
+	
+}
+}
