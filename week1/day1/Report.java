8c1f0db63f3b25ade30604d7327fcc3c348207b0
hariaki18 <haridosshari4@gmail.com>
2025-08-08 06:36:40 -0400

    Add files via upload

---
+++ week1/day1/Report.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Report {
+
+	public static void main(String[] args) {
+		Student s1 = new Student();
+		s1.studentName="hari";
+		s1.rollNumber=35;
+		s1.collegeName="mailam engineering college";
+		s1.markScored=70;
+		s1.cgpa=7.5f;
+		System.out.println("studentName:"+s1.studentName);
+		System.out.println("rollNo:"+s1.rollNumber);
+		System.out.println("collegeName:"+s1.collegeName);
+		System.out.println("markscored:"+s1.markScored);
+		System.out.println("cgpa"+s1.cgpa);
+		
+		
+
+	}
+
+}
---
+++ week1/day1/Student.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class Student {
+	
+	String studentName;
+	byte rollNumber;
+	String collegeName;
+	int markScored;
+	float cgpa;
+	
+
+}
