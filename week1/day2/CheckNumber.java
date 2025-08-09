db5dd268df3b084b3f188784873a6f7fcf73ded6
hariaki18 <haridosshari4@gmail.com>
2025-08-09 04:31:22 -0400

    Add files via upload

--- week1/day2/CheckNumber.java
+++ week1/day2/CheckNumber.java
@@ -3,15 +3,21 @@ import java.util.Scanner;
 public class CheckNumber {
 
 	public static void main(String[] args) {
+		// create scanner object
 		Scanner scan = new Scanner(System.in);
+		// print enter no
 		System.out.println("enter the number : ");
+		// get user input
 		int number =scan.nextInt();
+		//positive no , check the condition 
 		if(number>0) {
 			System.out.println("the number is positive");
 		}
+		// negative no, check the condition  
 		else if(number<0) {
 			System.out.println("the number is negative");
 		}
+		// both condition are not satisfaction come else
 		else {
 			System.out.println("the number neither positive or negative");
 		}
--- week1/day2/Palindrome.java
+++ week1/day2/Palindrome.java
@@ -3,7 +3,7 @@ package week1.day2;
 public class Palindrome {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
